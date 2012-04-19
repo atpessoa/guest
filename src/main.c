3d26f83b8b4d53687c5683fb8ce80e2c2763242d
Antonio Pessoa <atnpessoa@gmail.com>
2012-04-19 14:35:06 -0300

    Primeiro commit: testando 1, 2, 3.

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Arquivo: main.c
+ * Autor: Antônio Pessoa
+ * Data de criação: 19/04/2012
+ *
+ * Descrição: Arquivo para testes no git (github)
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello Git");
+
+    return 0;
+}
+
